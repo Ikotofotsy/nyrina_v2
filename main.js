c6736dcae74cdc884d0cfaa283fb1eaf7e988ae0
Ikotofotsy <nyrinaratovo@gmail.com>
2024-05-26 02:31:38 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("header").load("./head/head.html");
+    $("#about").load("./about/about.html");
+    $("#skill").load("./skill/skill.html");
+    $("#experience").load("./experience/experience.html");
+    $("#project").load("./project/project.html");
+});
